edd960ab75527bd114c03e7110116098b3c8f4b9
tjthanapat <thanapat-ja@hotmail.com>
2020-11-14 18:14:25 +0700

    Create README

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'host' => 'itf-tjthanapat-database.mysql.database.azure.com',
-    'username' => 'tjthanapat_admin@itf-tjthanapat-database',
-    'password' => 'itfDB2020',
-    'database' => 'itflab',
+    'host' => 'host',
+    'username' => 'username',
+    'password' => 'password',
+    'database' => 'databasename',
 );
 ?>
